95d9accc82a90dc543885675177fdd169b0ac874
NickGoit <113237474+NickGoit@users.noreply.github.com>
2022-09-11 12:40:10 +0300

    Updated
    1)повні назви змінних, не потрібно однією буквою називати
    2) по дефолту ітерація буде по ключам, тому можна не писати .keys()
    3) екземпляри класу Path, не потрібно їх ще раз обгортати в клас Path
    4) shutil.unpack_archive
    5) апустити без шляху (python main.py) наприклад, впаде IndexError :(

--- Home_Task_M6.py
+++ Home_Task_M6.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import sys
 import os
 import zipfile
+import shutil
 
 
 def normilise(line):
@@ -35,84 +36,85 @@ def sort_in_dir(dir_path):
                     'audio': ['.MP3', '.OGG', '.WAV', '.AMR'],
                     'archives': ['.ZIP', '.GZ', '.TAR']
                     }
-    p = Path(dir_path)  # get path object to calling dir
+    local_path_dir = Path(dir_path)  # get path object to calling dir
 
-    for file in p.iterdir():  # iterating under each file into directory
+    for file in local_path_dir.iterdir():  # iterating under each file into directory
 
-        file_path = Path.joinpath(p, file.name)  # make a path to file or dir
+        file_path = Path.joinpath(local_path_dir, file.name)  # make a path to file or dir
 
-        if file.is_dir() and (file.name not in known_suffix.keys()):
+        if file.is_dir() and (file.name not in known_suffix):
             if os.listdir(file_path):  # dir has at least on file
                 new_dir_name = normilise(file.name)  # make a new name for a dir
-                renamed_dir_path = Path.joinpath(p, new_dir_name)  # create a path to renamed dir
+                renamed_dir_path = Path.joinpath(local_path_dir, new_dir_name)  # create a path to renamed dir
                 os.rename(file_path, renamed_dir_path)
                 sort_in_dir(renamed_dir_path)
             else:
                 os.rmdir(file_path)
         else:
             if file.suffix.upper() in known_suffix['images']:
-                images_dir_path = os.path.join(p, 'images')  # create a path to a new dir
-                Path(images_dir_path).mkdir(parents=True, exist_ok=True)  # make a new dir based on path to this dir
+                images_dir_path = Path.joinpath(local_path_dir, 'images')  # create a path to a new dir
+                images_dir_path.mkdir(parents=True, exist_ok=True)  # make a new dir based on path to this dir
 
-                file_path_in_new_dir = Path.joinpath(Path(images_dir_path),
+                file_path_in_new_dir = Path.joinpath(images_dir_path,
                                                      file.name)  # create a file path into a new dir
                 os.replace(file_path, file_path_in_new_dir)  # replace file to a new dir
 
                 rename_file = normilise(file.name.replace(file.suffix, '')) + file.suffix  # make a new file name
-                renamed_file_path_in_new_dir = Path.joinpath(Path(images_dir_path),
+                renamed_file_path_in_new_dir = Path.joinpath(images_dir_path,
                                                              rename_file)   # create a path to renamed file
                 os.rename(file_path_in_new_dir, renamed_file_path_in_new_dir)  # rename file into new dir
 
                 picture_files.append(file.name)  # add to list
             elif file.suffix.upper() in known_suffix['video']:
-                video_dir_path = Path.joinpath(p, 'video')
-                Path(video_dir_path).mkdir(parents=True, exist_ok=True)
+                video_dir_path = Path.joinpath(local_path_dir, 'video')
+                video_dir_path.mkdir(parents=True, exist_ok=True)
 
-                file_path_in_new_dir = Path.joinpath(Path(video_dir_path), file.name)
+                file_path_in_new_dir = Path.joinpath(video_dir_path, file.name)
                 os.replace(file_path, file_path_in_new_dir)
 
                 rename_file = normilise(file.name.replace(file.suffix, '')) + file.suffix
-                renamed_file_path_in_new_dir = Path.joinpath(Path(video_dir_path), rename_file)
+                renamed_file_path_in_new_dir = Path.joinpath(video_dir_path, rename_file)
                 os.rename(file_path_in_new_dir, renamed_file_path_in_new_dir)  # rename file into new dir
 
                 video_files.append(file.name)
             elif file.suffix.upper() in known_suffix['documents']:
-                document_dir_path = Path.joinpath(p, 'documents')  # create a path to a new dir
-                Path(document_dir_path).mkdir(parents=True, exist_ok=True)  # make a new dir based on path to this dir
+                document_dir_path = Path.joinpath(local_path_dir, 'documents')  # create a path to a new dir
+                document_dir_path.mkdir(parents=True, exist_ok=True)  # make a new dir based on path to this dir
 
-                file_path_in_new_dir = Path.joinpath(Path(document_dir_path),
+                file_path_in_new_dir = Path.joinpath(document_dir_path,
                                                      file.name)  # create a file path into a new dir
                 os.replace(file_path, file_path_in_new_dir)  # replace file to a new dir
 
                 rename_file = normilise(file.name.replace(file.suffix, '')) + file.suffix  # make a new file name
-                renamed_file_path_in_new_dir = Path.joinpath(Path(document_dir_path),
+                renamed_file_path_in_new_dir = Path.joinpath(document_dir_path,
                                                              rename_file)  # create a path to renamed file
                 os.rename(file_path_in_new_dir, renamed_file_path_in_new_dir)
 
                 doc_files.append(file.name)
             elif file.suffix.upper() in known_suffix['audio']:
-                audio_dir_path = Path.joinpath(p, 'audio')
-                Path(audio_dir_path).mkdir(parents=True, exist_ok=True)
+                audio_dir_path = Path.joinpath(local_path_dir, 'audio')
+                audio_dir_path.mkdir(parents=True, exist_ok=True)
 
-                file_path_in_new_dir = Path.joinpath(Path(audio_dir_path), file.name)
+                file_path_in_new_dir = Path.joinpath(audio_dir_path, file.name)
                 os.replace(file_path, file_path_in_new_dir)
 
                 rename_file = normilise(file.name.replace(file.suffix, '')) + file.suffix  # make a new file name
-                renamed_file_path_in_new_dir = Path.joinpath(Path(audio_dir_path),
+                renamed_file_path_in_new_dir = Path.joinpath(audio_dir_path,
                                                              rename_file)  # create a path to renamed file
                 os.rename(file_path_in_new_dir, renamed_file_path_in_new_dir)
 
                 music_files.append(file.name)
 
             elif file.suffix.upper() in known_suffix['archives']:
-                archive_dir_path = Path.joinpath(p, 'archives')
-                Path(archive_dir_path).mkdir(parents=True, exist_ok=True)
-                sub_archive_dir_path = Path.joinpath(Path(archive_dir_path), file.name.replace(file.suffix, ''))
+                archive_dir_path = Path.joinpath(local_path_dir, 'archives')
+                archive_dir_path.mkdir(parents=True, exist_ok=True)
+                sub_archive_dir_path = Path.joinpath(archive_dir_path, file.name.replace(file.suffix, ''))
                 Path(sub_archive_dir_path).mkdir(parents=True, exist_ok=True)
-                new_file_path = Path.joinpath(Path(archive_dir_path), file.name)
+                new_file_path = Path.joinpath(archive_dir_path, file.name)
                 os.replace(file_path, new_file_path)
-                with zipfile.ZipFile(new_file_path, 'r') as zip_file:
-                    zip_file.extractall(sub_archive_dir_path)
+                #with zipfile.ZipFile(new_file_path, 'r') as zip_file:
+                #    zip_file.extractall(sub_archive_dir_path)
+                shutil.unpack_archive(new_file_path, sub_archive_dir_path)
                 archive_files.append(file.name)
             else:
                 unknown_file.append(file.name)
@@ -120,6 +122,12 @@ def sort_in_dir(dir_path):
 
 
 if __name__ == '__main__':
-    dir_0 = sys.argv[1]
-    sort_in_dir(dir_0)
-    print('Done!')
+    if len(sys.argv) < 2:
+        print('Please provide correct path')
+        exit()
+    current_path = sys.argv[1]
+    if not (os.path.exists(current_path) and Path(current_path).is_dir()):
+        print('inputed path is not existed or it\'s not a dir')
+        exit()
+    sort_in_dir(current_path)
+    print('Done!')  #
